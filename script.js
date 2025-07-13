2d25e091b6107a4d04161064e47262f35d3ecf0a
Sumitkumar2411 <sumitnandan2025@gmail.com>
2025-07-13 09:19:46 +0530

    form validation with use of regex

---
+++ script.js
@@ -0,0 +1,9 @@
+const userName = document.getElementById("username");
+const myContact = document.getElementById("contact");
+const myMail = document.getElementById("mail");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let messages = [];
+});
