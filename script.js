2e47ae96fdd74976dfbde884222eb7200a080fac
Sumitkumar2411 <sumitnandan2025@gmail.com>
2025-07-14 17:36:58 +0530

    strong password added

--- script.js
+++ script.js
@@ -1,9 +1,60 @@
 const userName = document.getElementById("username");
 const myContact = document.getElementById("contact");
 const myMail = document.getElementById("mail");
+const form = document.getElementById("registration-form");
+const error = document.getElementById("error");
+const passwordInput = document.getElementById("password");
+const submitBtn = document.getElementById("submitBtn");
+const msg = document.getElementById("msg");
+
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+
+passwordInput.addEventListener("input", () => {
+  const password = passwordInput.value.trim();
+
+  if (passwordRegex.test(password)) {
+    msg.textContent = "Strong password!";
+    msg.style.color = "green";
+  } else {
+    msg.textContent =
+      "Weak password! Use uppercase, lowercase, digit, special char & 8+ chars.";
+    msg.style.color = "red";
+  }
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   let messages = [];
+
+  if (!/^[A-Za-z\s]+$/.test(userName.value.trim())) {
+    messages.push("Name must contain only letters and spaces.");
+  }
+
+  if (!/^\d{10}$/.test(myContact.value.trim())) {
+    messages.push(
+      "Contact must be a 10-digit number with no spaces or special characters."
+    );
+  }
+
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+
+  if (!emailPattern.test(myMail.value.trim())) {
+    messages.push("Please enter a valid email address.");
+  }
+
+  if (!passwordRegex.test(passwordInput.value.trim())) {
+    messages.push("Please enter a strong password.");
+  }
+
+  if (messages.length > 0) {
+    error.innerText = messages.join(" ");
+  } else {
+    error.innerText = "";
+    alert("Form submitted successfully!");
+    form.reset();
+    msg.textContent = ""
+  }
 });
